b5446774e49993115c4629e464c364819e999188
Ricky-micky <erick.tembo@student.moringaschool.com>
2024-10-06 16:52:32 +0300

    added text

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// code your solution h
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    
+    if (winRecord) {
+      return winRecord.year; 
+    } else {
+      return undefined; 
+    }
+  }
+  
+  // Test the function
+  const gameRecords = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  console.log(superbowlWin(gameRecords)); // Outputs: "2015"
+  
